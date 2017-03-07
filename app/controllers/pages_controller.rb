a0604e8a212842b35d5dd670afc14bae34de9005
Yoo Taejin <taejin@Yooui-MacBook-Pro.local>
2017-03-07 11:04:08 +0900

    Global ranking displayed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,19 @@
 class PagesController < ApplicationController
-  def index
-  end
+ 	def index
+	  	girlCount = Girl.all.count
+
+	  	@globalRankScore = Array.new(girlCount, 0)	#Array for saving scores of each girl
+	  	resultSet = Hash.new
+
+	  	Ranking.find_each do |ranks|
+	  		#Girl who get rank 1 will get 8(Total number of girls - rank) points
+	  		@globalRankScore[ranks.girl_id - 1] += girlCount - ranks.rank
+	  	end
+
+	  	@globalRankScore.each_with_index do |score, index|
+	  		resultSet[index+1] = {:name => Girl.find_by(id: index+1).name, :score => score}
+	  	end
+
+	  	@resultSet = resultSet.sort_by { |k,v| v[:score] }.reverse
+	end
 end
