eba037f979f8b01088dc20191e14f35197605dad
Yoo Taejin <taejin@Yooui-MacBook-Pro.local>
2017-03-11 01:08:13 +0900

    ranking_controller tests

--- spec/controllers/ranking_controller_spec.rb
+++ spec/controllers/ranking_controller_spec.rb
@@ -18,18 +18,52 @@ RSpec.describe RankingController, type: :controller do
   end
 
   describe "POST #edit" do
+    let(:girls) {FactoryGirl.create_list(:girl,9)}
+    let(:user) {FactoryGirl.create(:user)}
+
     it "gives unranked girl a rank" do
-      girl = FactoryGirl.create(:girl)
-      user = FactoryGirl.create(:user)
+
       sign_in user
 
-      post :edit, params: { girl: girl.id , direction: "up" }
+      post :edit, params: { girl: girls[0].id , direction: "up" }
       expect(response).to have_http_status(:found)
 
       expect( user.rankings.count ).to eq(1)
 
     end
+    it "gives ranked girl a rank" do
+
+      sign_in user
+
+      post :edit, params: { girl: girls[0].id , direction: "up" }
+      post :edit, params: { girl: girls[1].id , direction: "up" }
+
+      post :edit, params: { girl: girls[1].id , direction: "up" }
+      expect(response).to have_http_status(:found)
+
+      expect( user.rankings.where( girl_id: girls[1].id).first.rank ).to eq(1)
+      expect( user.rankings.where( girl_id: girls[0].id).first.rank ).to eq(2)
+    end
+    it "gives 1st girl a rank up" do
+      
+      sign_in user
+      post :edit, params: { girl: girls[0].id , direction: "up" }
+
+      post :edit, params: { girl: girls[0].id , direction: "up" }
+      expect(response).to have_http_status(:found)
+
+      expect( user.rankings.where( girl_id: girls[0].id).first.rank ).to eq(1)
+    end
+    it "gives last girl a rank down" do   
+      sign_in user
+
+      for i in 0..8
+        post :edit, params: { girl: girls[i].id , direction: "up" }
+      end
+      post :edit, params: { girl: girls[8].id , direction: "down" }
+      expect(response).to have_http_status(:found)
+
+      expect( user.rankings.where( girl_id: girls[8].id).first.rank ).to eq(9)
+    end
   end
-
-
 end
