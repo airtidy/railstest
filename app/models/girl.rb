2f988a11ef51a80f8877b824946ad3f2a12345da
Kartikey Tanna <tannakartikey@gmail.com>
2017-07-01 00:17:15 +0530

    created method which sends girls name and global rank in desc order as hash

--- app/models/girl.rb
+++ app/models/girl.rb
@@ -7,4 +7,13 @@ class Girl < ApplicationRecord
 			ranking.rank
 		end
 	end
+
+  def self.with_global_ranks
+    result = []
+    Ranking.group('girl_id').select('SUM(rank) as global_rank, girl_id').order('global_rank desc')
+      .each do |e|
+        result.push ({:name => Girl.find(e.girl_id).name, :global_rank => e.global_rank})
+      end
+    result
+  end
 end
