dcafc71d5ab4f60e6ccefd0abfee36aad5cac55a
Ivan Dwijaya <ivan.chaz@gmail.com>
2017-07-01 23:34:07 +0700

    Add "best girl ranking view" and some bug fixes
    - Added "best girl ranking view" on the first page.
    - Fixed the button down would throw an error if the below girl has
      not been ranked
    - Fixed the button up would not do anything if the girl is the
      first one to be ranked.

--- app/models/ranking.rb
+++ app/models/ranking.rb
@@ -3,4 +3,8 @@ class Ranking < ApplicationRecord
   belongs_to :user
 
   validates_uniqueness_of :girl_id, scope: :user_id
+
+  def self.ladder
+    Ranking.joins(:girl).select("girls.name, sum(rankings.rank) as rank").group("girls.name").order("rank desc")
+  end
 end
