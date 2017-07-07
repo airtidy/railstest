49ab379682853564e3450ba7e5d2d4375d724053
Andres <abedandres@gmail.com>
2017-07-07 01:13:38 -0500

    Adds the method to get the best girls ranking
    Shows the best girls ranking into the index view

--- app/models/girl.rb
+++ app/models/girl.rb
@@ -1,4 +1,5 @@
 class Girl < ApplicationRecord
+  has_many :rankings
 	def rank(user)
 		ranking = Ranking.where(user: user, girl: self).first
 		if !ranking
@@ -7,4 +8,17 @@ class Girl < ApplicationRecord
 			ranking.rank
 		end
 	end
+
+  def self.global_ranking(girls)
+    ranking = []
+    girls.each do |girl|
+      value = 0
+      girl.rankings.each do |ranking|
+        value = value + ranking.rank
+      end
+      hash = {name: girl.name, rank: value}
+      ranking.push(hash)
+    end
+    return ranking.sort_by { |hash| hash[:rank] }
+  end
 end
