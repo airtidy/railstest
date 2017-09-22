9b63d51801d99727a66f39a8448d77a398af6e76
piercer <abaubkus@yahoo.com>
2017-09-22 13:12:50 +0900

    No more access to admin panel

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
 
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  #mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   
   root :to => "pages#index"
