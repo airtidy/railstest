020f924156772c0a62945316d19c73100f199304
BobiLissev <borislav.lissev@gmail.com>
2017-09-28 21:12:08 +0300

    Commit!

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,11 @@
 class PagesController < ApplicationController
   def index
+	@girls_ranking_list = Ranking.group(:girl_id).sum(:rank)
+	@girlArray = Array.new
+	@girls_ranking_list.each do |girlID|
+		@girlArray.push [Girl.select(:name).where(id: girlID[0]).first.attributes.values[1],
+						 @girls_ranking_list[girlID[0]]]
+	end
+	@girlArray.sort!{|x,y| y[1] <=> x[1]}  
   end
 end
