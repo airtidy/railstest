ff99942fb051709067bf8fabbbbcde935d733f9a
Pang Chong <pang.chong@rjc.co.jp>
2018-01-28 16:49:42 +0900

    implemented ranking algorithm

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
   def index
+  	@best_ranking = Ranking.best_nesoberi_ranking
   end
 end
--- app/models/ranking.rb
+++ app/models/ranking.rb
@@ -3,4 +3,28 @@ class Ranking < ApplicationRecord
   belongs_to :user
 
   validates_uniqueness_of :nesoberi_id, scope: :user_id
+
+  def self.best_nesoberi_ranking
+
+    num_of_rank_candi = Nesoberi.count
+    num_of_all_rank = Ranking.count
+    avg_num_of_rankings = (num_of_all_rank / num_of_rank_candi).to_f
+    avg_of_rankings =  (((num_of_all_rank * num_of_rank_candi) -  Ranking.sum(:rank)) / num_of_all_rank).to_f
+    # results of ranking
+    best_rankins_array = Hash.new
+
+    Nesoberi.all.each do | nesoberi |
+    	current_num_of_rankings = Ranking.where({nesoberi_id: nesoberi.id}).count
+    	if current_num_of_rankings > 1
+    		current_avg_of_rankings = num_of_rank_candi - Ranking.where({nesoberi_id: nesoberi.id}).average(:rank)
+    		# calculating bayesian avarage for ranking
+    		bayesian_average_ranking = (((avg_num_of_rankings * avg_of_rankings) + (current_num_of_rankings * current_avg_of_rankings)) / (avg_num_of_rankings + current_num_of_rankings)).to_f
+    		best_rankins_array[nesoberi.id] = { rank: bayesian_average_ranking, nesoberi_name:  nesoberi.name}
+    	else
+    		best_rankins_array[nesoberi.id] = { rank: 0 , nesoberi_name:  nesoberi.name}
+    	end
+    end
+    # desc sort ranking results
+    best_rankins_array.sort_by { | _ , v | -v[:rank]}
+  end
 end
